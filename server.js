6299cdbbbcf598ba0a86af7ccc831ed4187ff137
hitdatoc <hillary@secret6.com>
2017-05-08 19:17:00 +0800

    Copy index.js to server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var favicon = require('serve-favicon');
+
+app.get('/', function(request, response){
+	response.sendFile(__dirname + '/index.html');
+});
+
+app.use(favicon(__dirname + '/favicon.ico'));
+
+app.get('/favicon.ico', function(request, response){
+	response.sendFile(__dirname + '/favicon.ico');
+});
+
+http.listen(process.env.PORT || 8888);
