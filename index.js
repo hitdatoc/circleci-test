5289da5018f27b6616fc927fecc99642fa80a1b1
hitdatoc <hillary@secret6.com>
2017-05-08 19:03:59 +0800

    Add favicon.ico for heroku

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
+var favicon = require('serve-favicon');
 
 app.get('/', function(request, response){
 	response.sendFile(__dirname + '/index.html');
 });
+
+app.use(favicon(__dirname + '/favicon.ico'));
 
 http.listen(process.env.PORT || 8888);
